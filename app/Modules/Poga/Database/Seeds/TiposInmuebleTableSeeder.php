b3eb9733a3a81345a5ca01de2f71448702ade777
Mario Patronelli <mario@raffles.com.ar>
2019-08-07 14:24:54 +0000

    Corrige Duplex

--- app/Modules/Poga/Database/Seeds/TiposInmuebleTableSeeder.php
+++ app/Modules/Poga/Database/Seeds/TiposInmuebleTableSeeder.php
@@ -18,7 +18,7 @@ class TiposInmuebleTableSeeder extends Seeder
         $tiposInmueble = [
             ['tipo' => 'Edificio', 'descripcion' => 'Edificio', 'enum_aplica_a' => 'INMUEBLES', 'enum_estado' => 'ACTIVO', 'configurable_division_unidades' => 'true'],
             ['tipo' => 'Casa', 'descripcion' => 'Casa', 'enum_aplica_a' => 'INMUEBLES', 'enum_estado' => 'ACTIVO', 'configurable_division_unidades' => 'false'],
-            ['tipo' => 'Duplex', 'descripcion' => 'Duplex', 'enum_aplica_a' => 'NMUEBLES', 'enum_estado' => 'ACTIVO', 'configurable_division_unidades' => 'false', 'cant_pisos_fija' => '2'],
+            ['tipo' => 'Duplex', 'descripcion' => 'Duplex', 'enum_aplica_a' => 'INMUEBLES', 'enum_estado' => 'ACTIVO', 'configurable_division_unidades' => 'false', 'cant_pisos_fija' => '2'],
             ['tipo' => 'Triplex', 'descripcion' => 'Triplex', 'enum_aplica_a' => 'INMUEBLES', 'enum_estado' => 'ACTIVO', 'configurable_division_unidades' => 'false', 'cant_pisos_fija' => '3'],
             ['tipo' => 'Departamento', 'descripcion' => 'Departamento', 'enum_aplica_a' => 'UNIDADES', 'enum_estado' => 'ACTIVO', 'configurable_division_unidades' => 'false'],
             ['tipo' => 'Flat', 'descripcion' => 'Flat', 'enum_aplica_a' => 'UNIDADES', 'enum_estado' => 'ACTIVO', 'configurable_division_unidades' => 'false'],
