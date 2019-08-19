d9783aea50b7783047f44fb86e492445b69b4ba5
Mario Patronelli <mario@raffles.com.ar>
2019-08-19 15:43:23 +0000

    Agrega MantenimientoController

---
+++ app/Modules/Poga/Http/Controllers/MantenimientoController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace Raffles\Modules\Poga\Http\Controllers\Mantenimientos;
+
+use Raffles\Modules\Poga\Repositories\UserRepository;
+use Raffles\Modules\Poga\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+use RafflesArgentina\ResourceController\Traits\FormatsValidJsonResponses;
+
+class MantenimientoController extends Controller
+{
+    use FormatsValidJsonResponses;
+
+    /**
+     * The UserRepository object.
+     *
+     * @var UserRepository $repository
+     */
+    protected $repository;
+
+    /**
+     * Create a new MantenimientoController instance.
+     *
+     * @param UserRepository $repository The UserRepository object.
+     *
+     * @return void
+     */
+    public function __construct(UserRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $model = $this->repository->findOrFail($request->idInmueblePadre);
+
+        $model->load('mantenimientos');
+
+        $items = $model->mantenimientos->where('enum_estado', 'ACTIVO');
+
+        return $this->validSuccessJsonResponse('Success', $items);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  int                      $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
