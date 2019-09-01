f82463f86af0a33ac107d99fd8060075c7add631
Mario Patronelli <mario@raffles.com.ar>
2019-09-01 20:59:05 +0000

    Actualiza rutas

--- app/Modules/Poga/Routes/api.php
+++ app/Modules/Poga/Routes/api.php
@@ -10,50 +10,53 @@
 | is assigned the 'api' middleware group. Enjoy building your API!
 |
 */
-Route::get('proveedores', ProveedorController::class);
-Route::get('dashboard', DashboardController::class);
-Route::get('inmuebles/personaReferenteDadoRol', Inmuebles\PersonaReferenteDadoRolController::class);
-Route::get('inmuebles/monedas', Inmuebles\MonedaController::class);
-Route::get('inmuebles/tiposUnidades', Inmuebles\TipoUnidadController::class);
-Route::get('reservas/espaciosComunes', Reservas\EspacioComunController::class);
-//Route::get('finanzas/pagos', Finanzas\PagoController::class);
-Route::get('finanzas/posiblesAcreedores', Finanzas\PosibleAcreedorController::class);
-Route::get('finanzas/posiblesDeudores', Finanzas\PosibleDeudorController::class);
-Route::get('finanzas/tiposPagares', Finanzas\TipoPagareController::class);
-Route::get('proveedores', ProveedorController::class);
-Route::get('solicitudes/inmueble/agendadas', Solicitudes\AgendadaInmuebleController::class);
-//Route::get('solicitudes/proveedor/agendadas', Solicitudes\AgendadaProveedorController::class);
-Route::get('solicitudes/proveedoresDadoServicio', Solicitudes\ProveedorDadoServicioController::class);
-Route::get('solicitudes/servicios', Solicitudes\ServicioController::class);
-Route::get('solicitudes/serviciosDeProveedor', Solicitudes\ServicioProveedorController::class);
-Route::get('solicitudes/inmueble/sinAgendar', Solicitudes\SinAgendarInmuebleController::class);
-//Route::get('solicitudes/proveedor/sinAgendar', Solicitudes\SinAgendarProveedorController::class);
-Route::get('solicitudes/proveedoresDadoServicio', Solicitudes\ProveedorDadoServicioController::class);
+
+// Auth
+Route::get('usuario-invitado/{codigo_validacion}', 'Auth\UsuarioInvitadoController');
 
 Route::post('login', 'Auth\LoginController@login');
-Route::post('seleccionar-rol', Roles\SeleccionarRolController::class);
+Route::post('register', 'Auth\RegisterController@register');
 
-Route::put('inmuebles/desvincular', Inmuebles\DesvincularController::class);
+Route::put('registro-invitado/{codigo_validacion}', 'Auth\GuestRegisterController@register');
+Route::post('seleccionar-rol', Auth\SeleccionarRolController::class);
 
-Route::apiResource('ciudades-cobertura', CiudadCoberturaController::class);
-Route::apiResource('inmuebles', Inmuebles\InmuebleController::class);
-Route::apiResource('mantenimientos', Mantenimientos\MantenimientoController::class);
-Route::apiResource('nacionalidades', NacionalidadController::class);
-Route::apiResource('nominaciones', Nominaciones\NominacionController::class);
-Route::apiResource('paises-cobertura', PaisCoberturaController::class);
-Route::apiResource('reservas', Reservas\ReservaController::class);
+Route::apiResource('ciudades-cobertura', Auth\CiudadCoberturaController::class);
+Route::apiResource('paises', PaisController::class, ['only' => ['index']]);
+Route::apiResource('paises-cobertura', Auth\PaisCoberturaController::class);
+Route::apiResource('roles', Auth\RolController::class);
+
+// Dashboard
+Route::get('dashboard', DashboardController::class);
+
+// Espacios
+Route::apiResource('espacios', EspacioController::class);
+
+// Eventos
+Route::apiResource('reservas', Eventos\ReservaController::class);
+Route::apiResource('visitas', Eventos\VisitaController::class);
+
+// Finanzas
+Route::apiResource('monedas', Finanzas\MonedaController::class);
 Route::apiResource('rentas', Finanzas\RentaController::class);
-Route::apiResource('tipos-caracteristica', Inmuebles\TipoCaracteristicaController::class);
-Route::apiResource('visitas', Visitas\VisitaController::class);
-Route::apiResource('rentas', RentaController::class);
 
-/**
- * Revisados
- */
-Route::apiResource('formatos', FormatoController::class);
+// Inmuebles
+Route::put('inmuebles/desvincular', Inmuebles\DesvincularController::class);
+Route::apiResource('formatos', Inmuebles\FormatoController::class);
+Route::apiResource('inmuebles', Inmuebles\InmuebleController::class);
 Route::apiResource('medidas', Inmuebles\MedidaController::class, ['only' => 'index']);
-Route::apiResource('personas', PersonaController::class);
-Route::apiResource('tipos-inmueble', TipoInmuebleController::class);
-Route::apiResource('roles', Roles\RolController::class);
-Route::apiResource('solicitudes/proveedor', Solicitudes\ProveedorController::class);
+Route::apiResource('inmueble-personas', Inmuebles\PersonaController::class);
+Route::apiResource('tipos-caracteristica', Inmuebles\TipoCaracteristicaController::class);
+Route::apiResource('tipos-inmueble', Inmuebles\TipoInmuebleController::class);
 Route::apiResource('unidades', Inmuebles\UnidadController::class);
+
+// Mantenimientos
+Route::apiResource('mantenimientos', Mantenimientos\MantenimientoController::class);
+
+// Nominaciones
+Route::apiResource('nominaciones', Nominaciones\NominacionController::class);
+
+// Personas
+Route::apiResource('personas', PersonaController::class);
+
+// Proveedores
+Route::apiResource('proveedores', ProveedorController::class);
